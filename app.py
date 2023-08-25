cf2fe3942431927fae7920e67dcd35e54425431a
chgsvc2 <142673501+chgsvc2@users.noreply.github.com>
2023-08-25 13:07:54 +0530

    Deleted unwanted lines of code

--- app.py
+++ app.py
@@ -10,23 +10,10 @@ import os
 from PIL import Image
 import base64
 import random
-
+from utils import main_model,get_random_image,message,upload,process_line,process_image_with_yolo,process_image_with_yolo_pic1,process_image_with_yolo_pic2
 
 st.set_page_config(layout="wide",initial_sidebar_state="expanded",
                    page_icon='🔎',page_title='Poth-Hole Detector')
-
-image_directory = "val"  # Assuming "val" is the directory name
-
-# Get a list of image filenames in the directory
-image_filenames = [filename for filename in os.listdir(image_directory) if filename.endswith(".jpg")]
-
-# Function to generate a random image from the list of filenames
-def get_random_image():
-    if not image_filenames:
-        return None
-    random_image_filename = random.choice(image_filenames)
-    random_image_path = os.path.join(image_directory, random_image_filename)
-    return random_image_path      
 
 # Define custom style for the glowing text
 glowing_text_style = '''
@@ -52,46 +39,9 @@ glowing_text_style = '''
 st.markdown(glowing_text_style, unsafe_allow_html=True)
 st.markdown(f'<p class="glowing-text">🕳️ PothHole Detector 🕳️</p>', unsafe_allow_html=True)
 
-def upload():
-    image=None
-    image_filename=None
-    initial_image = st.camera_input('Take a picture')
-    original_image = initial_image
-    temp_path = None
-    if initial_image is not None:
-        image_filename = f"{int(time.time())}.jpg"
-        bytes_data = initial_image.getvalue()
-        image = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
 
-    return image, original_image,image_filename
 
-def process_line(line, image_np,counter):
-    # Process a single line from the labels.txt file
-    bresults = line.split()
-    if len(bresults) >=5:
-        names={0:'POTH_HOLE'}
-        xc, yc, nw, nh = map(float, bresults[1:5])
-        h, w = image_np.shape[0], image_np.shape[1]
-
-        xc *= w
-        yc *= h
-        nw *= w
-        nh *= h
-        top_left = (int(xc - nw / 2), int(yc - nh / 2))
-        bottom_right = (int(xc + nw / 2), int(yc + nh / 2))
-
-        # Draw bounding box
-        cv2.rectangle(image_np, top_left, bottom_right, (4, 29, 255), 3, cv2.LINE_4)
-
-        # Draw label text
-        #label = names[int(bresults[0])]
-        label = f'{names[int(bresults[0])]}-{counter}'
-        text_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 2, 3)[0]
-        text_width, text_height = text_size
-        text_x = (top_left[0] + bottom_right[0] - text_width) // 2 + 100
-        text_y = top_left[1] - 10
-        cv2.putText(image_np, label, (text_x, text_y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
-        
+       
 
  
 sidebar_option = st.sidebar.radio("Select an option", ("Take picture for prediction", "Upload file"))
@@ -107,55 +57,12 @@ def main():
     
         
             image, original_image,image_filename= upload()
-            if original_image is not None and original_image is not None and len(image_filename)!=0 and st.button('Prediction'):  # Check if original_image is not None
+            if original_image is not None and image_filename is not None and len(image_filename)!=0 and st.button('Prediction'):  # Check if original_image is not None
                 st.info('Wait for the results...!')
                 #image1=cv2.imread(image)
-                
-                counter=1
-                names={0:'POTH_HOLE'
-                    }
-               
-                model=YOLO('best.pt')
-                result = model.predict(image,save=True,save_txt=True)
-                txt_files_exist = any(filename.endswith('.txt') for filename in os.listdir('runs/detect/predict/labels'))
-                if txt_files_exist:
-                    lis=open('runs/detect/predict/labels/image0.txt','r').readlines()
-                    for line in lis:
-                        process_line(line, image,counter)
-                        counter+=1
-                    with st.spinner('Wait for the results...!'):
-                        time.sleep(5)
-                    
-                    st.image(image,use_column_width=True)
-                    st.balloons()
-                    
-                    try:
-                        if os.path.exists('runs'):
-                            shutil.rmtree('runs')
-                            st.session_state.original_image = None  # Clear the original_image variable
-                           
-                    except Exception as e:
-                        st.error(f"An error occurred: {e}")
-                else:
-                    st.warning('⚠️Please check your image')
-                    st.info("📷✨ **Encountering the 'Please check your image' error?**")
-                    st.write(
-                            """
-                            Our algorithm may not have been able to predict the content of your image. To improve results, consider the following:
-                            👉 **Verify image quality and resolution.**
-                            👉 **Ensure the image is clear and well-lit.**
-                            👉 **Check if the image meets our specified format requirements.**
-                            👉 **Consider alternative images for better results.**
-                            Our aim is to provide accurate predictions, and addressing these aspects can make a significant difference. If the issue persists, please reach out to our support team. We're here to help! 🤝🔧
-                            """
-                        )
-                    try:
-                        if os.path.exists('runs'):
-                            shutil.rmtree('runs')
-                            st.session_state.original_image = None  # Clear the original_image variable
-                           
-                    except Exception as e:
-                        st.error(f"An error occurred: {e}")
+                pic0=image
+                process_image_with_yolo(pic0)
+
     elif sidebar_option == "Upload file":  
           
         
@@ -169,119 +76,25 @@ def main():
             if True:
                     
                 if fileimage is None:
-                    default_image=get_random_image()
+                    default_image,image_name=get_random_image()
                     st.warning('⚠️ We are using random image from our backend!.')
-                    st.info('Wait for the results...!')
-                    counter=1
-                    pic=Image.open(default_image)
-                    image_np = np.array(pic)
-                    names={0:'POTH_HOLE'
-                        }
-                    mod1=YOLO('best.pt')
-                    mod1.predict(image_np,save=True,save_txt=True)
-                    txt_files_exist = any(filename.endswith('.txt') for filename in os.listdir('runs/detect/predict/labels'))
-                    if txt_files_exist:
-                        lis=open('runs/detect/predict/labels/image0.txt','r').readlines()
-                        with st.spinner('Wait for the results...!'):
-                            time.sleep(5)
-    
-                                    
-                        for line in lis:
-                            process_line(line, image_np,counter)
-                            counter+=1
-                        col1,col2=st.columns(2)
-                        with col1:
-                                
-                            st.info('Original Image!')
-                            st.image(default_image,use_column_width=True)
-                        with col2:
-                            st.info('Detected Image!')
-                            st.image(image_np,use_column_width=True)
-                        st.balloons()
+                    
+                    
+                    pic1=Image.open(default_image)
+                    image_np1 = np.array(pic1)
+                    if pic1 is not None and image_np1 is not None:
                         
-                        try:
-                            if os.path.exists('runs'):
-                                shutil.rmtree('runs')
-                                st.session_state.original_image = None  # Clear the original_image variable
-                                
-                        except Exception as e:
-                            st.error(f"An error occurred: {e}")
-                    else:
-                        st.warning('⚠️Please check your image')
-                        st.info("📷✨ **Encountering the 'Please check your image' error?**")
-                        st.write(
-                            """
-                            Our algorithm may not have been able to predict the content of your image. To improve results, consider the following:
-                            👉 **Verify image quality and resolution.**
-                            👉 **Ensure the image is clear and well-lit.**
-                            👉 **Check if the image meets our specified format requirements.**
-                            👉 **Consider alternative images for better results.**
-                            Our aim is to provide accurate predictions, and addressing these aspects can make a significant difference. If the issue persists, please reach out to our support team. We're here to help! 🤝🔧
-                            """
-                        )
-                        try:
-                            if os.path.exists('runs'):
-                                shutil.rmtree('runs')
-                                st.session_state.original_image = None  # Clear the original_image variable
-                            
-                        except Exception as e:
-                            st.error(f"An error occurred: {e}")
+                        process_image_with_yolo_pic1(pic1,image_np1,image_name)
+                    
                 else:
                     st.info('Wait for the results...!')
-                    counter=1
-                    pic=Image.open(fileimage)
-                    image_np = np.array(pic)
-                    names={0:'POTH_HOLE'
-                        }
-                    mod1=YOLO('best.pt')
-                    mod1.predict(image_np,save=True,save_txt=True)
-                    txt_files_exist = any(filename.endswith('.txt') for filename in os.listdir('runs/detect/predict/labels'))
-                    if txt_files_exist:
-                        lis=open('runs/detect/predict/labels/image0.txt','r').readlines()
-                        with st.spinner('Wait for the results...!'):
-                            time.sleep(5)
-    
-                                    
-                        for line in lis:
-                            process_line(line, image_np,counter)
-                            counter+=1
-                        col1,col2=st.columns(2)
-                        with col1:
-                                
-                            st.info('Original Image!')
-                            st.image(fileimage,use_column_width=True)
-                        with col2:
-                            st.info('Detected Image!')
-                            st.image(image_np,use_column_width=True)
-                        st.balloons()
-                        
-                        try:
-                            if os.path.exists('runs'):
-                                shutil.rmtree('runs')
-                                st.session_state.original_image = None  # Clear the original_image variable
-                                
-                        except Exception as e:
-                            st.error(f"An error occurred: {e}")
-                    else:
-                        st.warning('⚠️Please check your image')
-                        st.info("📷✨ **Encountering the 'Please check your image' error?**")
-                        st.write(
-                            """
-                            Our algorithm may not have been able to predict the content of your image. To improve results, consider the following:
-                            👉 **Verify image quality and resolution.**
-                            👉 **Ensure the image is clear and well-lit.**
-                            👉 **Check if the image meets our specified format requirements.**
-                            👉 **Consider alternative images for better results.**
-                            Our aim is to provide accurate predictions, and addressing these aspects can make a significant difference. If the issue persists, please reach out to our support team. We're here to help! 🤝🔧
-                            """
-                        )
-                        try:
-                            if os.path.exists('runs'):
-                                shutil.rmtree('runs')
-                                st.session_state.original_image = None  # Clear the original_image variable
-                            
-                        except Exception as e:
-                            st.error(f"An error occurred: {e}")
+                    
+                    
+                    pic2=Image.open(fileimage)
+                    image_np2 = np.array(pic2)
+                    if pic2 is not None and image_np2 is not None:
+                        process_image_with_yolo_pic2(fileimage,image_np2)
+                   
                     
 
         
